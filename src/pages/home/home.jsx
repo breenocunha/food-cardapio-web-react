db3de016d6c2fec2b72daf16354016f7885fe4c1
Breno Cunha <154290956+breenocunha@users.noreply.github.com>
2024-01-25 18:10:23 -0300

    Initial commit

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,31 @@
+import Navbar from "../../components/navbar/navbar.jsx";
+import ProdutoVitrine from "../../components/produto-vitrine/produto-vitrine.jsx";
+import {produtos} from "../../dados.js";
+
+function Home(){
+    return <>
+        <Navbar showMenu={true} />
+
+        <div className="container">
+            <div className="titulo text-center">
+                <h1>Nosso Cardápio</h1>
+                <p className="subtitulo">Clique em adicionar para colocar os produtos na sacola de compras. Se preferir, você pode pedir pelo WhatsApp: (91) 98542-3182</p>
+            </div>
+        </div>
+        
+        <div className="text-center">
+            {
+                produtos.map(function(prod){
+                    return <ProdutoVitrine key={prod.id}
+                                           id={prod.id}
+                                           nome={prod.nome}
+                                           descricao={prod.descricao}
+                                           preco={prod.preco}
+                                           foto={prod.foto}  />
+                })                
+            }
+        </div>
+    </>
+}
+
+export default Home;
